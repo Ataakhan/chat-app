8d538293ccc109dd38885b59598e7dfbd7aff327
Ataa <ataa@gmail.com>
2023-07-09 20:24:33 +0530

    firebase setup

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCxrmwl4D25T51AMMPFIwWg9RYFRWZZ2Iw',
+  authDomain: 'chat-web-app-db635.firebaseapp.com',
+  projectId: 'chat-web-app-db635',
+  storageBucket: 'chat-web-app-db635.appspot.com',
+  messagingSenderId: '1084931046089',
+  appId: '1:1084931046089:web:a30a0975925e8fd611eb18',
+};
+
+const app = firebase.initializeApp(config);
