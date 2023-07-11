ded88f70bf509dcf6f35bddcfbd6b3df4dae3f2f
Ataa <ataa@gmail.com>
2023-07-11 12:00:27 +0530

    public and private route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import 'rsuite/dist/rsuite.min.css';
+import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </switch>
+  );
 }
 
 export default App;
