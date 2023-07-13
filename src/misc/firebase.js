8c15dccbb835a94e2ec576ce6ec9be97b3e017e7
Ataa <ataa@gmail.com>
2023-07-13 10:11:42 +0530

    firebase interaction with sign in page

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyCxrmwl4D25T51AMMPFIwWg9RYFRWZZ2Iw',
@@ -10,3 +12,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
